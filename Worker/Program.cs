800227d8dd96f64bee2d4cc4da4174176116b143
Guillermo Correa <luis.guillermo.correa@hotmail.com>
2013-06-08 17:31:04 -0500

    Added Job

--- Worker/Program.cs
+++ Worker/Program.cs
@@ -12,6 +12,7 @@ namespace Worker
     {
         private static readonly ISchedulerFactory SchedulerFactory;
         private static readonly IScheduler Scheduler;
+        private static IJobDetail _emailJobDetail;
 
         static Program()
         {
@@ -22,7 +23,38 @@ namespace Worker
 
         static void Main(string[] args)
         {
-            
+            // Now let's start our scheduler; you could perform
+            // any processing or bootstrapping code here before
+            // you start it but it must be started to schedule
+            // any jobs
+            Scheduler.Start();
+
+            // Let's generate our email job detail now
+            CreateJob();
+        }
+
+        private static void CreateJob()
+        {
+
+            // The job builder uses a fluent interface to
+            // make it easier to build and generate an
+            // IJobDetail object
+            _emailJobDetail = JobBuilder.Create<EmailJob>()
+                .WithIdentity("SendToMyself")   // Here we can assign a friendly name to our job        
+                .Build();                       // And now we build the job detail
+
+        }
+    }
+
+    /// <summary>
+    /// Our email job, yet to be implemented
+    /// </summary>
+    public class EmailJob : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            // TODO: Implement this
+            throw new System.NotImplementedException();
         }
     }
 }
